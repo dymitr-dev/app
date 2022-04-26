b1f78f995c5503ac06e2b0e55a0e4918505831c9
dymitr-dev <contact@dymitr.dev>
2022-04-27 00:56:46 +0200

    Return 400 if there is no route

--- src/main/java/dev/dymitr/app/controller/RouteController.java
+++ src/main/java/dev/dymitr/app/controller/RouteController.java
@@ -3,6 +3,8 @@ package dev.dymitr.app.controller;
 import dev.dymitr.app.model.Route;
 import dev.dymitr.app.service.RouteService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,7 +15,9 @@ public class RouteController {
     private RouteService routeService;
 
     @GetMapping("/routing/{origin}/{destination}")
-    public Route getRoute(@PathVariable String origin, @PathVariable String destination) {
-        return routeService.getRoute(origin, destination);
+    public ResponseEntity<Route> getRoute(@PathVariable String origin, @PathVariable String destination) {
+        Route route = routeService.getRoute(origin, destination);
+        HttpStatus httpStatus = route.getRoute().isEmpty() ? HttpStatus.NOT_FOUND : HttpStatus.OK;
+        return new ResponseEntity<>(route, httpStatus);
     }
 }
--- src/main/java/dev/dymitr/app/service/RouteService.java
+++ src/main/java/dev/dymitr/app/service/RouteService.java
@@ -2,10 +2,12 @@ package dev.dymitr.app.service;
 
 import dev.dymitr.app.model.Route;
 import org.jgrapht.Graph;
+import org.jgrapht.GraphPath;
 import org.jgrapht.alg.shortestpath.DijkstraShortestPath;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -15,7 +17,8 @@ public class RouteService {
 
     public Route getRoute(String origin, String destination) {
         DijkstraShortestPath dijkstraShortestPath = new DijkstraShortestPath(graph);
-        List<String> shortestPath = dijkstraShortestPath.getPath(origin, destination).getVertexList();
+        GraphPath graphPath = dijkstraShortestPath.getPath(origin, destination);
+        List<String> shortestPath = graphPath == null ? Collections.EMPTY_LIST : graphPath.getVertexList();
         return Route.builder().route(shortestPath).build();
     }
 }
